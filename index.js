12ad6b863e2bf625ecceb381352f5e147e185d1f
Julio <julioantjimenez@gmail.com>
2023-08-15 10:03:54 -0400

    object lab assignment

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
-// Write your solution in this file!
+const employee = {
+    name: "John",
+    streetAddress: "Some Street Drive",
+};
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newObj = {...employee, "name": "Sam", "streetAddress": "11 Broadway"}
+return newObj
+}
+
+beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function deleteFromEmployeeByKey(employee, key) {
+    const newObject = { ...employee };
+    delete newObject[key];
+    return newObject;
+  }
+
+  beforeEach(function () {
+    for (const key in employee) {
+      delete employee[key];
+    }
+
+    employee.name = 'Sam';
+  });
+
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
